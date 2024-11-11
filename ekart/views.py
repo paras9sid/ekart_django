59babcd1417b1b4ec2e2a17166a8dfcf799bc132
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-11 15:14:28 +0530

    first commit

---
+++ ekart/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    # return HttpResponse("<h1>Homepage</h1>")
+    return render(request,'home.html')
