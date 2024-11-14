03cffb509aa4d4633bedfe8d5f3191ab44dbad83
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-14 21:10:25 +0530

    Product model created,add model products, products display in homepage

--- ekart/views.py
+++ ekart/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from store.models import Product
 
 
 def home(request):
-    # return HttpResponse("<h1>Homepage</h1>")
-    return render(request,'home.html')
+    # query set
+    products = Product.objects.all().filter(is_available=True)
+
+    # dictionary for rendering data in html to frontend
+    context = {
+        'products': products,
+    }
+    return render(request,'home.html', context)
